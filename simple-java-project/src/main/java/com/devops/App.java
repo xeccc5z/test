7b097a236cc00f4bb7e63dd1bb1b0e44ff0e0c7d
xeccc5z <balaji168@gmail.com>
2021-06-29 02:29:14 +0000

    mvn install executed

--- simple-java-project/src/main/java/com/devops/App.java
+++ simple-java-project/src/main/java/com/devops/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Hi Balaji" );
+	System.out.println( "Hi Mahesh" );
     }
 }
