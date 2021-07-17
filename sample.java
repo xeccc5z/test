6ddfd785f5b4c9191c7cbd5c4d3af0babbfc0370
xeccc5z <85573473+xeccc5z@users.noreply.github.com>
2021-07-17 07:29:45 +0530

    create sample file

---
+++ sample.java
@@ -0,0 +1,15 @@
+package com.devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hi Balaji" );
+	System.out.println( "Hi Mahesh" );
+    }
+}
