49bc812e3c9bbae44eea8e1f2ce4fd6cde4d2cdb
xeccc5z <85573473+xeccc5z@users.noreply.github.com>
2021-07-17 07:56:09 +0530

    updated dinesh

--- sample.java
+++ sample.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hi Balaji" );
+        System.out.println( "Hi Dinesh" );
 	System.out.println( "Hi Mahesh" );
     }
 }
